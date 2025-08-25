667fa0c1499645d3a2d95c6c80ea55cd3f675afd
thisalma <alwisthisalma@gamil/.com>
2025-08-25 14:48:02 +0530

    updating the models😍

--- foodies/app/Models/Order.php
+++ foodies/app/Models/Order.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+     protected $fillable = ['user_id', 'total_price', 'status'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
